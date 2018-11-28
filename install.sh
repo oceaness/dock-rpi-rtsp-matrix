3855f30ecaa07d5c1c0ac728af56c53044f4bad1
oceaness <jack@jtester.co.uk>
2018-11-28 16:24:59 +0000

    install.sh

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# This script will automatically set your raspberry Pi's GPU memory
+# split to 256mb, this should be sufficient for most feed layouts.
+# You may decrease this but you may experience graphical errors.
+
+gpu_mem="256"
+
+# We will also be disabling overscan which will remove black
+# borders from most modern screens.
+
+# Finally we will install docker if it is not already installed and
+# add the Pi user to the docker user group.
+
+
+# If /boot/config.txt contains "gpu_mem"
+if grep -Fq gpu_mem /boot/config.txt
+then
+	# Replace it with new value
+	sudo sed -i "/gpu_mem=/c\gpu_mem=$gpu_mem" /boot/config.txt
+else
+	# Append it
+	sudo sh -c "echo '\ngpu_mem=$gpu_mem' >> /boot/config.txt"
+fi
+
+# If boot/config.txt contains "disable_overscan"
+if grep -Fq disable_overscan /boot/config.txt
+then
+	# Replace it with new value
+	sed -i '/disable_overscan/c\disable_overscan=1' /boot/config.txt
+else
+	# Append it
+	sudo sh -c "echo '\ndisable_overscan=1' >> /boot/config.txt"
+fi
+
+# If docker is not installed
+if ! docker -v; then
+	# Install docker
+	curl -sSL https://get.docker.com | sh
+fi
+
+# If user pi is not in docker group
+if ! groups pi | grep -q 'docker'; then
+	# Add default pi user to docker group
+	sudo usermod -aG docker pi
+fi
+
+echo "Please reboot your pi for the settings to take effect"
