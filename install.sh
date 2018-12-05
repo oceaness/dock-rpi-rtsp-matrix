4b68a22657c62b647799b5fa59ccfa829e253e5e
oceaness <jack@jtester.co.uk>
2018-12-05 12:41:02 +0000

    Code tidy

--- install.sh
+++ install.sh
@@ -2,20 +2,19 @@
 
 # This script will automatically set your raspberry Pi's GPU memory
 # split to 256mb, this should be sufficient for most feed layouts.
-# You may decrease this but you may experience graphical errors.
+# You may decrease this if you have lower feed numbers.
 
 gpu_mem="256"
 
 # We will also be disabling overscan which will remove black
 # borders from most modern screens.
 
-# Finally we will install docker if it is not already installed and
-# add the Pi user to the docker user group.
+# Finally we will install docker, if it is not already installed and
+# add the pi user to the docker user group.
 
 
 # If /boot/config.txt contains "gpu_mem"
-if grep -Fq gpu_mem /boot/config.txt
-then
+if grep -Fq gpu_mem /boot/config.txt; then
 	# Replace it with new value
 	sudo sed -i "/gpu_mem=/c\gpu_mem=$gpu_mem" /boot/config.txt
 else
@@ -24,8 +23,7 @@ else
 fi
 
 # If boot/config.txt contains "disable_overscan"
-if grep -Fq disable_overscan /boot/config.txt
-then
+if grep -Fq disable_overscan /boot/config.txt; then
 	# Replace it with new value
 	sudo sed -i '/disable_overscan/c\disable_overscan=1' /boot/config.txt
 else
@@ -34,7 +32,7 @@ else
 fi
 
 # If docker is not installed
-if ! docker -v &> /dev/null; then
+if ! docker -v > /dev/null 2>&1; then
 	# Install docker
 	curl -sSL https://get.docker.com | sh
 fi
@@ -47,7 +45,7 @@ fi
 
 # Check if pi user password has been changed
 if [ -e /run/sshwarn ] ; then
-	echo "The password for the user 'pi' is still the default value, please change this by running passwd"
+	printf "The password for the user 'pi' is still the default value, please change this by running passwd\n\n"
 fi
 
 echo "Please reboot your pi for the settings to take effect"
